c07459b066172fc5b6606deebe93ef7bf49c1785
Bratin Kundu <36078208+bratinkundu@users.noreply.github.com>
2020-10-03 11:20:21 +0530

    Delete StringToBase64.js

--- StringToBase64.js
+++
@@ -1,9 +0,0 @@
-let str = "Learning JavaScript"; 
-
-// encoding the string
-let result = window.btoa(str);
-console.log(result);
-
-// decoding the string
-let result1 = window.atob(result);
-console.log(result1);
