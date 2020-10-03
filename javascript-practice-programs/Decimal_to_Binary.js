cf9d3ceeecdf922e81423c15392050d01666e2ca
AbhijayJain <64387054+Abhijay007@users.noreply.github.com>
2020-10-03 14:02:58 +0530

    Update Decimal_to_Binary.js
    added user input method

--- javascript-practice-programs/Decimal_to_Binary.js
+++ javascript-practice-programs/Decimal_to_Binary.js
@@ -1,19 +1,35 @@
-var decimal = 22
-var binary = []
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Binary to Decimal with Different Approach</title>
+</head>
+
+<body>
+    <script>
+        var decimal = prompt("enter decimal number")
+        var binary = []
 
 
-while (decimal > 0) {
+        while (decimal > 0) {
 
-    if (decimal % 2 != 0) {
-        binary.push(1)
-        decimal = (decimal - 1) / 2
-    } else {
-        binary.push(0)
-        decimal = (decimal) / 2
-    }
+            if (decimal % 2 != 0) {
+                binary.push(1)
+                decimal = (decimal - 1) / 2
+            } else {
+                binary.push(0)
+                decimal = (decimal) / 2
+            }
 
-}
+        }
 
-binary = binary.reverse()
-binary = binary.join("")
-console.log(binary)
+        binary = binary.reverse()
+        binary = binary.join("")
+        document.write(binary)
+        
+    </script>
+</body>
+
+</html>
